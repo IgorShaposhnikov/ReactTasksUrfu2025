44a207f0a3ed76b46a6d3b2c5287ce92a9eed0bd
Igor Shaposhnikov <Igor.Shaposhnikov@urfu.me>
2025-11-22 11:52:45 +0500

    Add Todo deleteHandler function with console.log

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -14,11 +14,15 @@ function Todo(props) {
     setShowModal(false);
   }
 
+  function deleteHandler() {
+      console.log("Button clicked");
+  }
+
   return (
     <div className='card'>
       <h2>{props.title}</h2>
       <div className='actions'>
-        <button className='btn' onClick={showModalHandler}>
+        <button className='btn' onClick={deleteHandler}>
           Delete
         </button>
       </div>
